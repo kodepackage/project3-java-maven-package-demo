68a2d5324ec1c32379f13d06b0a6297e5c716fba
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-12 19:37:52 +0530

    update.

--- src/main/java/in/koderoom/HelloWorld.java
+++ src/main/java/in/koderoom/HelloWorld.java
@@ -3,5 +3,12 @@ package in.koderoom;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello World");
+        String str = sayHello();
+        System.out.println(str);
+    }
+
+
+    public static String sayHello() {
+        return "Helloooooo";
     }
 }
